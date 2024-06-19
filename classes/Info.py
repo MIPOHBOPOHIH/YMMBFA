ee81944be867847ceb481c3ec51fd5c173cbd15f
MIPOHBOPOHIH <mipoh@mipoh.ru>
2024-06-19 21:19:11 +0300

    first commit

---
+++ classes/Info.py
@@ -0,0 +1,228 @@
+from fastapi import HTTPException
+from math import floor
+from random import random
+from classes.Radio import Radio
+
+
+class Info:
+    def __init__(self, client, lastfm_username=None, lastfm_network=None):
+        self.client = client
+        self.radio = Radio(client)
+        self.first_track = None
+        self.lastfm_username = lastfm_username
+        self.lastfm_network = lastfm_network
+
+    @staticmethod
+    async def get_img_uri(cover_uri: str) -> str:
+        return f"https://{cover_uri[:-2]}1000x1000"
+
+    @staticmethod
+    async def get_download_link(data) -> str:
+        try:
+            return (await data.get_download_info_async(get_direct_links=True))[0].direct_link
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch download link") from e
+
+    @staticmethod
+    async def get_artists(data) -> str:
+        return ', '.join(data.artists_name())
+
+    async def get_track_info(self, track):
+        try:
+            duration = track.duration_ms // 1000
+            return {
+                'track_id': int(track.track_id.split(':')[0]),
+                'title': track.title,
+                'artist': (await self.get_artists(track)),
+                'img': (await self.get_img_uri(track.cover_uri)),
+                'duration': track.duration_ms // 1000,
+                'minutes': duration // 60,
+                'seconds': duration % 60,
+                'album': track.albums[0].id,
+                'download_link': (await self.get_download_link(track))
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch track info") from e
+
+    async def get_track_by_id(self, track_id: int):
+        try:
+            track = await self.client.tracks([track_id])
+            return await self.get_track_info(track[0])
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch track info by ID") from e
+
+    async def get_favourite_songs(self, skip, count):
+        playlist = await self.client.users_likes_tracks()
+        playlist = await playlist.fetch_tracks_async()
+        total = len(playlist)
+        playlist = playlist[skip:]
+        playlist = playlist[:count]
+        data = {
+            'skipped': skip,
+            'count': count,
+            'total': total,
+            'tracks': []
+        }
+        for track in playlist:
+            track1 = await self.get_track_info(track)
+            data['tracks'].append(track1)
+        return data
+
+    async def get_album_info(self, album):
+        try:
+            tracks = []
+            for track in album.volumes[0]:
+                track_id = int(track.track_id.split(':')[0])
+                tracks.append(track_id)
+            return {
+                'title': album.title,
+                'artists': ', '.join(album.artists_name()),
+                'track_count': album.track_count,
+                'img': (await self.get_img_uri(album.cover_uri)),
+                'tracks': tracks
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch album info") from e
+
+    async def get_albums_with_tracks(self, album_id):
+        try:
+            album = await self.client.albums_with_tracks(album_id)
+            return await self.get_album_info(album)
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch album info") from e
+
+    async def get_track_playlist_of_day(self):
+        feed = (await self.client.feed()).generated_playlists
+        tracks = []
+        for playlists in feed:
+            if playlists.type == 'playlistOfTheDay':
+                data = playlists.data.tracks
+                for track in data:
+                    track = await track.fetch_track_async()
+                    new_track = await self.get_track_info(track)
+                    tracks.append(new_track)
+        return tracks
+
+    async def search(self, request):
+        try:
+            searching_data = await self.client.search(request)
+            best = None
+            if searching_data['best']['type'] == 'artist':
+                best = await self.get_artist_info(searching_data['best']['result']['id'])
+            elif searching_data['best']['type'] == 'track':
+                best = await self.get_track_info(searching_data['best']['result'])
+            elif searching_data['best']['type'] == 'album':
+                best = await self.get_album_info(searching_data['best']['result'])
+            tracks = []
+            track_search = await self.client.search(type_='track', text=request)
+            track_tasks = [self.get_track_by_id(track['id']) for track in track_search['tracks']['results']]
+            return {
+                'type': searching_data['best']['type'],
+                'best': best,
+                'tracks': tracks
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to search") from e
+
+    async def getTrackFromStation(self):  # создание случайного радио и брать трек оттуда
+        _stations = await self.client.rotor_stations_list()
+        _station_random_index = floor(len(_stations) * random())
+        _station = _stations[_station_random_index].station
+        _station_id = f'{_station.id.type}:{_station.id.tag}'
+        _station_from = _station.id_for_from
+        track = await self.radio.start_radio(_station_id, _station_from)
+        return await self.get_track_info(track)
+
+    async def getNewReleases(self, skip, count):
+        new_releases = (await self.client.new_releases()).to_dict()
+        new_releases = new_releases['new_releases']
+        new_releases = new_releases[skip:]
+        new_releases = new_releases[:count]
+        releases = []
+        for release in new_releases:
+            data = await self.get_albums_with_tracks(release)
+            releases.append(data)
+        return releases
+
+    async def get_current_track(self):
+        if self.lastfm_network is not None:
+            data = await self.lastfm_network.user_get_recent_tracks(self.lastfm_username, limit=1)
+            artist_name = data['recenttracks']['track'][0]['artist']['name']
+            track_name = data['recenttracks']['track'][0]['name']
+            album_name = data['recenttracks']['track'][0]['album']['#text']
+            searching_track = await self.client.search(f'{artist_name} {track_name} {album_name}')
+            current_track = searching_track['best']['result']
+            await self.lastfm_network._session.close()  # че за хуйня, пукчарм жалуется
+            return await self.get_track_info(current_track)
+        queues = await self.client.queues_list()
+        last_queue = await self.client.queue(queues[0].id)
+        last_track_id = last_queue.get_current_track()
+        current_track = await last_track_id.fetch_track_async()
+        return await self.get_track_info(current_track)
+
+    async def get_artist_info(self, artist_id):
+        try:
+            artist = (await self.client.artists(artist_id))[0]
+            artist_tracks = await self.client.artists_tracks(artist_id)
+            artist_albums = await self.client.artists_direct_albums(artist_id)
+            return {
+                'id': artist['id'],
+                'name': artist['name'],
+                'cover_url': f"https://{artist['cover']['uri'][:-2]}1000x1000",
+                'genres': artist['genres'],
+                'albums': [album.id for album in artist_albums.albums],
+                'tracks': [int(track.track_id.split(':')[0]) for track in artist_tracks.tracks]
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to fetch artist info") from e
+
+    async def like_track(self, track_id):
+        try:
+            return {
+                "message": await self.client.users_likes_tracks_add(track_id)
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to like track") from e
+
+    async def unlike_track(self, track_id):
+        try:
+            return {
+                "message": await self.client.users_likes_tracks_remove(track_id)
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to unlike track") from e
+
+    async def like_album(self, album_id):
+        try:
+            return {
+                "message": await self.client.users_likes_albums_add(album_id)
+            }
+        except Exception as e:
+            raise HTTPException(status_code=500,
+                                detail="Failed to like album") from e
+
+    async def get_like_tracks_by_username(self, username, skip, count):
+        playlist = await self.client.users_likes_tracks(username)
+        playlist = await playlist.fetch_tracks_async()
+        total = len(playlist)
+        playlist = playlist[skip:]
+        playlist = playlist[:count]
+        data = {
+            'skipped': skip,
+            'count': count,
+            'total': total,
+            'tracks': []
+        }
+        for track in playlist:
+            track1 = await self.get_track_info(track)
+            data['tracks'].append(track1)
+        return data
---
+++ main.py
@@ -0,0 +1,235 @@
+from fastapi import FastAPI, Query
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+from yandex_music import ClientAsync as Client
+from classes.Info import Info
+import lastfm
+import random
+import json
+import aiohttp
+import string
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+client_key = config['LASTFM_API_KEY']
+client_secret = config['LASTFM_API_SECRET']
+app = FastAPI()
+title_string = "<title>YMMBFA - Swagger UI</title>"
+
+
+@app.get("/song/{track_id}")
+async def get_song_by_id(track_id: int,
+                         ya_token: str = Query(...,
+                                               title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_track_by_id(track_id)
+
+
+@app.get("/songs")  # ПРИМЕР track_ids: 73131629,73131630
+async def get_tracks_by_ids(track_ids: str,
+                            ya_token: str = Query(...,
+                                                  title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    data = track_ids.split(',')
+    return [await info.get_track_by_id(track) for track in data]
+
+
+@app.get("/favourite_songs")
+async def get_favourite_tracks(skip: int = 0, count: int = 25, ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_favourite_songs(skip, count)
+
+
+@app.get("/album/{album_id}")
+async def get_album_by_id(album_id: int, ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_albums_with_tracks(album_id)
+
+
+@app.get("/playlist_of_the_day")
+async def get_tracks_from_playlist_of_the_day(ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_track_playlist_of_day()
+
+
+@app.get("/search")
+async def get_search(request: str, ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.search(request)
+
+
+@app.get("/get_track_from_station")
+async def getTrackFromStation(ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.getTrackFromStation()
+
+
+@app.get("/new_release")
+async def get_new_release(skip: int = 0, count: int = 10, ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.getNewReleases(skip, count)
+
+
+@app.get("/current_track")
+async def get_current_track(ya_token: str = Query(..., title="Yandex Music Token"),
+                            lastfm_username: str = Query(None, title="Yandex Music Token")):
+    network = None
+    if lastfm_username:
+        network = lastfm.Client(
+            client_key=client_key,
+            client_secret=client_secret)
+    client = await Client(ya_token).init()
+    info = Info(client, lastfm_network=network, lastfm_username=lastfm_username)
+    return await info.get_current_track()
+
+
+@app.get("/artist/{artist_id}")
+async def get_album(artist_id: int,
+                    ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_artist_info(artist_id)
+
+
+@app.get("/like_track/{track_id}")
+async def like_track(track_id: int,
+                     ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.like_track(track_id)
+
+
+@app.get("/dislike_track/{track_id}")
+async def dislike_track(track_id: int,
+                        ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.unlike_track(track_id)
+
+
+@app.get("/get_current_track_beta")
+async def get_current_track_very_beta(token: str = Query(..., title="Yandex Music Token")):
+    device_info = {
+        "app_name": "Chrome",
+        "type": 1,
+    }
+
+    ws_proto = {
+        "Ynison-Device-Id": "".join([random.choice(string.ascii_lowercase) for _ in range(16)]),
+        "Ynison-Device-Info": json.dumps(device_info)
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(
+                url="wss://ynison.music.yandex.ru/redirector.YnisonRedirectService/GetRedirectToYnison",
+                headers={
+                    "Sec-WebSocket-Protocol": f"Bearer, v2, {json.dumps(ws_proto)}",
+                    "Origin": "http://music.yandex.ru",
+                    "Authorization": f"OAuth {token}",
+                },
+        ) as ws:
+            recv = await ws.receive()
+            data = json.loads(recv.data)
+
+        new_ws_proto = ws_proto.copy()
+        new_ws_proto["Ynison-Redirect-Ticket"] = data["redirect_ticket"]
+
+        to_send = {
+            "update_full_state": {
+                "player_state": {
+                    "player_queue": {
+                        "current_playable_index": -1,
+                        "entity_id": "",
+                        "entity_type": "VARIOUS",
+                        "playable_list": [],
+                        "options": {
+                            "repeat_mode": "NONE"
+                        },
+                        "entity_context": "BASED_ON_ENTITY_BY_DEFAULT",
+                        "version": {
+                            "device_id": ws_proto["Ynison-Device-Id"],
+                            "version": 9021243204784341000,
+                            "timestamp_ms": 0
+                        },
+                        "from_optional": ""
+                    },
+                    "status": {
+                        "duration_ms": 0,
+                        "paused": True,
+                        "playback_speed": 1,
+                        "progress_ms": 0,
+                        "version": {
+                            "device_id": ws_proto["Ynison-Device-Id"],
+                            "version": 8321822175199937000,
+                            "timestamp_ms": 0
+                        }
+                    }
+                },
+                "device": {
+                    "capabilities": {
+                        "can_be_player": True,
+                        "can_be_remote_controller": False,
+                        "volume_granularity": 16
+                    },
+                    "info": {
+                        "device_id": ws_proto["Ynison-Device-Id"],
+                        "type": "WEB",
+                        "title": "Chrome Browser",
+                        "app_name": "Chrome"
+                    },
+                    "volume_info": {
+                        "volume": 0
+                    },
+                    "is_shadow": True
+                },
+                "is_currently_active": False
+            },
+            "rid": "ac281c26-a047-4419-ad00-e4fbfda1cba3",
+            "player_action_timestamp_ms": 0,
+            "activity_interception_type": "DO_NOT_INTERCEPT_BY_DEFAULT"
+        }
+
+        async with session.ws_connect(
+                url=f"wss://{data['host']}/ynison_state.YnisonStateService/PutYnisonState",
+                headers={
+                    "Sec-WebSocket-Protocol": f"Bearer, v2, {json.dumps(new_ws_proto)}",
+                    "Origin": "http://music.yandex.ru",
+                    "Authorization": f"OAuth {token}",
+                },
+                method="GET"
+        ) as ws:
+            await ws.send_str(json.dumps(to_send))
+            recv = await ws.receive()
+            ynison = json.loads(recv.data)
+            print(ynison)
+            track = ynison['player_state']['player_queue']['playable_list'][
+                ynison['player_state']['player_queue']['current_playable_index']]
+        await session.close()
+        client = await Client(token).init()
+        info = Info(client)
+        return await info.get_track_by_id(track['playable_id'])
+
+
+@app.get("/get_likes_from_username")
+async def get_likes_from_username(username: str, skip: int = 0, count: int = 10,
+                                  ya_token: str = Query(..., title="Yandex Music Token")):
+    client = await Client(ya_token).init()
+    info = Info(client)
+    return await info.get_like_tracks_by_username(username, skip, count)
+
+
+app.mount("/", StaticFiles(directory="./static/", html=True))
+
+if __name__ == '__main__':
+    import uvicorn
+
+    try:
+        uvicorn.run(app, host="127.0.0.1", port=8000)
+    except KeyboardInterrupt:
+        exit()
