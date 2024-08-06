0d77642bad6f8b9efbdca989040858372024ae8e
MIPOHBOPOHIH <98014624+MIPOHBOPOHIH@users.noreply.github.com>
2024-08-06 22:37:40 +0300

    127.0.0.1 -> LOCALHOST

--- main.py
+++ main.py
@@ -224,6 +224,6 @@ if __name__ == '__main__':
     import uvicorn
 
     try:
-        uvicorn.run(app, host="127.0.0.1", port=8000)
+        uvicorn.run(app, host="0.0.0.0", port=8000)
     except KeyboardInterrupt:
         exit()
